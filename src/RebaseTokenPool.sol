cc5513490a556cc72168d69c3bd272f8d763e456
locbgiang <locbgiang@gmail.com>
2025-08-26 17:13:34 -0700

    RebaseTokenPool

--- src/RebaseTokenPool.sol
+++ src/RebaseTokenPool.sol
@@ -8,7 +8,49 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
 
 contract RebaseTokenPool is TokenPool {
-    // constructor to initialize the tokenpool
-    // lock or burn function
+    
+    constructor (
+        IERC20 token, 
+        uint8 localTokenDecimal, 
+        address[] memory allowlist, 
+        address rmnProxy, 
+        address router
+    ) TokenPool(token, 8, allowlist, rmnProxy, router) {}
+
+    // This function will be called by the CCIP Router
+    function lockOrBurn(Pool.LockOrBurnInV1 calldata lockOrBurnIn)
+        external    // only CCIP Router can call this
+        virtual     // Future contracts can override this function
+        override    // This replaces the parent function
+        returns(Pool.LockOrBurnOutV1 memory lockOrBurnOut)
+    {
+        // validate the input paremeters
+        _validateLockOrBurn(lockOrBurnIn);
+
+        // get the user interest rate from the original sender
+        uint256 userInterestRate = IRebaseToken(address(i_token)).getUserInterestRate(lockOrBurnIn.originalSender);
+        
+        // 
+        IRebaseToken(address(i_token)).burn(address(this), lockOrBurnIn.amount);
+
+        lockOrBurnOut = Pool.LockOrBurnOutV1({
+            destTokenAddress: getRemoteToken(lockOrBurnIn.remoteChainSelector),
+            destPoolData: abi.encode(userInterestRate)
+        });
+    } 
+
     // release or mint function
+    function releaseOrMint(Pool.ReleaseOrMintInV1 calldata releaseOrMintIn)
+        external
+        returns (Pool.ReleaseOrMintOutV1 memory)
+    {
+        _validateReleaseOrMint(releaseOrMintIn);
+        address receiver = releaseOrMintIn.receiver;
+        (uint256 userInterestRate) = abi.decode(releaseOrMintIn.sourcePoolData, (uint256));
+        IRebaseToken(address(i_token)).mint(receiver, releaseOrMintIn.amount, userInterestRate);
+
+        return Pool.ReleaseOrMintOutV1({
+            destinationAmount: releaseOrMintIn.amount
+        });
+    }
 }
